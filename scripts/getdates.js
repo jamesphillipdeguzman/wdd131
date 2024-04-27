fcb26022b1e90958b2d9770f5d601aeabfc710f4
jamesphillipdeguzman <deg24001@byui.edu>
2024-04-28 01:06:43 +0800

    My home page for WDD 131

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+// use the date object
+const today = new Date();
+const year = document.querySelector("#currentyear");
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
